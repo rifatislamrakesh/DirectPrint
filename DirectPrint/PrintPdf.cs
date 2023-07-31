456ce79514a80860e857ec68f601735b03775fb8
Rifat <rifatislamrakesh@gmail.com>
2023-07-31 20:28:27 +0600

    Add project files.

---
+++ DirectPrint/PrintPdf.cs
@@ -0,0 +1,23 @@
+﻿using Spire.Pdf;
+
+namespace DirectPrint
+{
+    public static class PrintPdf
+    {
+        public static bool Print(string pdfFilePath)
+        {
+            try
+            {
+                PdfDocument doc = new();
+                doc.LoadFromFile(pdfFilePath);
+                doc.Print();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ DirectPrint/Program.cs
@@ -0,0 +1,7 @@
+﻿using DirectPrint;
+
+string pdfFileName = "C:\\Users\\rifat\\source\\repos\\DirectPrint\\DirectPrint\\Pdfs\\file.pdf"; // Replace with the actual PDF file name
+
+Console.WriteLine(PrintPdf.Print(pdfFileName));
+
+Console.ReadLine();
